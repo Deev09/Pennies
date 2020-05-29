c2eba54212a10ae3583b5238ddb7fcf58a1060ca
Deev <deevyaswain@gmail.com>
2020-05-29 12:32:54 +0400

    Multiple users visualized

--- multipleuser.py
+++ multipleuser.py
@@ -1,9 +1,11 @@
 # Naive algorithm for problem solving
 import random
 import string
+import matplotlib.pyplot as plt
+import numpy as np
+from collections import Counter 
 
-
-def match(pattern,trials):
+def match(pattern,trials,arr):
     for i in range(trials):
         scores=[]
         A_sequence=len(pattern[0])
@@ -11,39 +13,59 @@ def match(pattern,trials):
         listo=[]
         option=['H','T']
         for i in range(1):
-            listo.append(''.join([random.choice(option) for n in range(32)]))   
+            listo.append(''.join([random.choice(option) for n in range(32)]))
+          
     
         for position in range(len(listo)):
             for i in range(len(pattern)):
                 for indexA in range(random_string-A_sequence+1):
                     j=0
                     while j<A_sequence:
+                        
+                            
                         if listo[position][indexA+j]!=pattern[i][j]:
                             break
+                     
                         j+=1
-                        
+                     
                     if j==A_sequence:
-                        print('pattern1 found at index',indexA)
+                        #print('pattern1 found at index',indexA)
+                        
                         scores.append(indexA)
                         break
-                                    
-        print(listo)          
+                    elif  j>len(listo):
+                        indexA=32
+                        scores.append(indexA)
+                        break
+                                   
+        #print(listo)          
         print(scores)
-        
-            
-        
-        print('player',scores.index(min(scores))+1,'wins')
+        match.winner=scores.index(min(scores))+1
+        arr.append(match.winner)
+       
+        print('player',match.winner,'wins')
+    print(arr)
    
 
-
+supreme=[]
 
 players=[]
-n=int(input())
+n=int(input('How many player?'))
 for i in range(n):
-    pattern_n=input()
+    pattern_n=input('input player:')
     players.append(pattern_n)
 
 num=int(input('how many rounds?'))
-match(players,num)
 
+match(players,num,supreme)
 
+labels = [i for i in range(1,n+1)]
+values=[]
+for i in labels:
+    d=Counter(supreme)
+    match=d[i]
+    values.append(match)
+plt.figure(figsize=(9, 3))
+plt.bar(labels, values)
+plt.show()
+    
